2111b460af6e39c062e5452b7f519d8f7f001e00
Muhammad Irfan Akhtar <irfanakhtar345@hotmail.com>
2020-11-15 19:14:26 +0000

    auto mapper added

---
+++ InsurwaveCodeTestAPI/AutoMapper/WeatherDetail_Profile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsurwaveCodeTestAPI.AutoMapper
+{
+    public class WeatherDetail_Profile: Profile
+    {
+        public WeatherDetail_Profile()
+        {
+            CreateMap<Category, Create_CategoryViewModel>()
+              .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Name))
+              .ForMember(dest => dest.CurrentDateTimeUtc, opt => opt.MapFrom(src => src.CreatedByDateTime))
+              .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.CreatedBy))
+              .ReverseMap();
+        }
+    }
+}
