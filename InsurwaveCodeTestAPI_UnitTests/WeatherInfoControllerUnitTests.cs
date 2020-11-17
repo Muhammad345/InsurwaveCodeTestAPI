cf17f3d49bb3d053a9138f686abe1248dce96578
Muhammad Irfan Akhtar <irfanakhtar345@hotmail.com>
2020-11-17 11:54:13 +0000

    Added some green test to check basic functionality

--- InsurwaveCodeTestAPI_UnitTests/WeatherInfoControllerUnitTests.cs
+++ InsurwaveCodeTestAPI_UnitTests/WeatherInfoControllerUnitTests.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using CityWeatherDetail;
+using Data.Constant;
 using Data.Response;
 using InsurwaveCodeTestAPI.Controllers;
 using Microsoft.AspNetCore.Mvc;
@@ -27,9 +28,10 @@ namespace InsurwaveCodeTestAPI_UnitTests
         private WeatherInfoController SUT;
         private CurrentDetail _currentDetail;
         private CityWeatherInfo _cityWeatherInfo;
-        
-
-
+        private CityDetailWithTempUnit _cityDetailWithTempUnit;
+        private Location _location;
+        private Current _current;
+      
         [SetUp]
         public void Setup()
         {
@@ -37,19 +39,24 @@ namespace InsurwaveCodeTestAPI_UnitTests
             _mockinsurwaveWeatherInfoService = new Mock<IInsurwaveWeatherInfoService>();
             _mockmapper = new Mock<IMapper>();
 
+            _current = new Current
+            {
+                TempC = "11.0",
+                TempF = "54.3"
+            };
+
+            _location = new Location
+            {
+                Name = "London",
+                Country = "United Kingdom",
+                Localtime = "2020-11-16 13:02",
+                Region = "City of London, Greater London"
+            };
+
             _currentDetail = new CurrentDetail
             {
-                 Current = new Current
-                 {
-                     TempC = "11.0",
-                     TempF = "54.3"
-                 },
-                  Location = new Location { 
-                   Name = "London",
-                   Country = "United Kingdom",
-                   Localtime = "2020-11-16 13:02",
-                   Region  = "City of London, Greater London"
-                  }
+                 Current = _current,
+                 Location = _location
             };
 
             _astronomyDetail = new AstronomyDetail {
@@ -60,12 +67,7 @@ namespace InsurwaveCodeTestAPI_UnitTests
                          Sunset ="04:09 PM"
                      }
                  },
-                Location = new Location
-                {
-                    Country = "United Kingdom",
-                    Localtime = "2020-11-16 13:02",
-                    Region = "City of London, Greater London"
-                }
+                Location = _location
             };
 
             _city = "London";
@@ -80,11 +82,21 @@ namespace InsurwaveCodeTestAPI_UnitTests
                 Temperature = _currentDetail.Current.TempC
             };
 
+            _cityDetailWithTempUnit = new CityDetailWithTempUnit
+            {
+                City = _currentDetail.Location.Name,
+                Country = _currentDetail.Location.Country,
+                LocalTime = _currentDetail.Location.Localtime,
+                Region = _currentDetail.Location.Region
+            };
+
+            
+
             WeatherApiHttpClientResponse = new WeatherApiHttpClientResponse
             {
             Data = JsonConvert.SerializeObject(_currentDetail),
             IsSuccessFull = true,
-            StatusCode = System.Net.HttpStatusCode.OK
+            StatusCode = HttpStatusCode.OK
             };
 
             AstronomyWeatherApiHttpClientResponse = new WeatherApiHttpClientResponse
@@ -97,8 +109,10 @@ namespace InsurwaveCodeTestAPI_UnitTests
             _mockinsurwaveWeatherInfoService.Setup(x => x.GetLocalWeatherInfo(_city)).Returns(Task.FromResult(WeatherApiHttpClientResponse));
 
             _mockinsurwaveWeatherInfoService.Setup(x => x.GetAstronomyInfo(_city, _date)).Returns(Task.FromResult(AstronomyWeatherApiHttpClientResponse));
-          //  _moockLogger.Setup(x => x.LogError(It.IsAny<string>(), It.IsAny<Object>()));
+            _mockinsurwaveWeatherInfoService.Setup(x => x.GetAstronomyInfo(_city, null)).Returns(Task.FromResult(AstronomyWeatherApiHttpClientResponse));
+            //  _moockLogger.Setup(x => x.LogError(It.IsAny<string>(), It.IsAny<Object>()));
             _mockmapper.Setup(x => x.Map<CityWeatherInfo>(It.IsAny<CurrentDetail>())).Returns(_cityWeatherInfo);
+            _mockmapper.Setup(x => x.Map<CityDetailWithTempUnit>(It.IsAny<CurrentDetail>())).Returns(_cityDetailWithTempUnit);
 
             SUT = new WeatherInfoController(_moockLogger.Object, _mockinsurwaveWeatherInfoService.Object, _mockmapper.Object);
 
@@ -113,14 +127,42 @@ namespace InsurwaveCodeTestAPI_UnitTests
             //Act
             var result = await SUT.Get(_city, _date);
             var okObjectResult = result as OkObjectResult;
-
-
+            var cityWeatherInfo = okObjectResult.Value as CityWeatherInfo;
 
             //Assert
-
             Assert.IsNotNull(result);
-            Assert.AreEqual(okObjectResult.StatusCode,HttpStatusCode.OK);
-           
+            Assert.AreEqual((int)HttpStatusCode.OK, okObjectResult.StatusCode);
+            Assert.AreEqual(_city, cityWeatherInfo.City);
+            Assert.AreEqual(_location.Localtime, cityWeatherInfo.LocalTime);
+            Assert.AreEqual(_location.Region, cityWeatherInfo.Region);
+            Assert.AreEqual(_location.Country, cityWeatherInfo.Country);
+            Assert.AreEqual(_astronomyDetail.Astronomy.Astro.Sunrise, cityWeatherInfo.Sunrise);
+            Assert.AreEqual(_astronomyDetail.Astronomy.Astro.Sunset, cityWeatherInfo.Sunset);
+        }
+
+        [TestCase("London", InsurwaveConstants.TemperatureMeasurement.Celsius, "11.0")]
+        [TestCase("London", InsurwaveConstants.TemperatureMeasurement.Fahrenheit, "54.3")]
+        public async Task GetCurrentWeatherInfoWithSpecicyMeasurement_Successfull(string city,string tempMeasuringUnit,string expectedTemp)
+        {
+            //Arrange 
+            
+
+            //Act
+            var result = await SUT.Get(city, tempMeasuringUnit);
+            var okObjectResult = result as OkObjectResult;
+            var cityWeatherInfo = okObjectResult.Value as CityDetailWithTempUnit;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual((int)HttpStatusCode.OK, okObjectResult.StatusCode);
+            Assert.AreEqual(_city, cityWeatherInfo.City);
+            Assert.AreEqual(_location.Localtime, cityWeatherInfo.LocalTime);
+            Assert.AreEqual(_location.Region, cityWeatherInfo.Region);
+            Assert.AreEqual(_location.Country, cityWeatherInfo.Country);
+            Assert.AreEqual(_astronomyDetail.Astronomy.Astro.Sunrise, cityWeatherInfo.Sunrise);
+            Assert.AreEqual(_astronomyDetail.Astronomy.Astro.Sunset, cityWeatherInfo.Sunset);
+            Assert.AreEqual(expectedTemp, cityWeatherInfo.Temperature);
+            Assert.AreEqual(tempMeasuringUnit, cityWeatherInfo.TempMeasurementInUnit);
         }
     }
 }
